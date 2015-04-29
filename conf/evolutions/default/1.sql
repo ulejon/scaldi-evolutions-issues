d8427b624d641e45c79af85e5736516467af469b
ulrikl <ulrik.lejon@gmail.com>
2015-04-29 15:23:10 +0200

    changed to inmemory db

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,10 +1,8 @@
 # --- !Ups
 CREATE TABLE `mytable` (
   `allgood`      TINYINT(1)  UNSIGNED NOT NULL  DEFAULT '0'
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_unicode_ci;
+);
+
 
 # --- !Downs
 DROP TABLE `mytable`;
